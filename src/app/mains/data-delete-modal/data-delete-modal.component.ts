bb3ead679a16cccfccb621da11d1681d4f7fb222
金子丘 <great_kaneko@kanekotakashinoMacBook-Air.local>
2020-09-21 09:28:16 +0900

    JLXメイン削除登録 削除者セット修正 入力担当者--->ログインユーザー氏名

--- src/app/mains/data-delete-modal/data-delete-modal.component.ts
+++ src/app/mains/data-delete-modal/data-delete-modal.component.ts
@@ -3,8 +3,10 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { MatDialog } from '@angular/material';
 import { KanriService } from '../../service/kanri.service';
 import { KanriTableService } from '../../service/kanri-table.service';
+import { SessionService } from '../../service/session.service';
 import { Kanri } from '../../class/kanri';
 import { Const } from '../../class/const';
+import { Tantousha } from '../../class/tantousha';
 import { PopupAlertComponent } from '../../popup/popup-alert/popup-alert.component';
 import { FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
 
@@ -14,7 +16,8 @@ import { FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
   styleUrls: ['./data-delete-modal.component.css']
 })
 export class DataDeleteModalComponent implements OnInit {
-  message: string;                                          // エラーメッセージ
+  message: string;                                          // エラーconsolelog用
+  loginUser = new Tantousha();                              // ログインユーザー情報用
   //sakujyoriyuReadOnly = '';                               // 削除理由有無 初期値無し
   //buttonDisabled = false;                                 // 実行ボタン可能判定
   formGroup: FormGroup;                                     // フォームグループ、以下フォームコントロール初期化
@@ -26,15 +29,17 @@ export class DataDeleteModalComponent implements OnInit {
               @Inject(MAT_DIALOG_DATA) public data: Kanri,  // data-->インジェクション書類データ
               private kanriService: KanriService,
               private kanriTableService: KanriTableService,
+              private sessionService: SessionService,
               private fb: FormBuilder,
               private popupAlertDialog: MatDialog,
   ) { }
 
   ngOnInit() {
+    this.loginUser = this.sessionService.setLoginUser();      // ログインユーザー情報取得
     this.data = this.kanriTableService.getSelected();         // インジェクションに選択された書類データ初期セット
     if (this.data.status === Const.STATUS_NG) {               // Status3=不備の時、以下組み合わせ
       //this.sakujyoriyuReadOnly = null;                      // 削除理由フォームを入力可能とする
-      //this.buttonDisabled = true;                             // 実行ボタン不可（削除理由入力後可能)
+      //this.buttonDisabled = true;                           // 実行ボタン不可（削除理由入力後可能)
       this.sakujyoriyu = new FormControl('', { validators: [Validators.required] });  // 削除理由フォーム作成必須条件とする
     } else {
       this.sakujyoriyu = new FormControl('');                 // 削除理由フォーム作成必須条件なし
@@ -55,7 +60,7 @@ export class DataDeleteModalComponent implements OnInit {
   */
   public deleteKanri() {
     this.data.sakujyoriyu = this.formGroup.value.sakujyoriyu; // 削除理由セット
-    this.data.sakujyosha = this.data.tantousha;               // 担当者IDセット
+    this.data.sakujyosha = this.loginUser.shimei;             // 担当者セット
     // DB編集更新処理
     this.kanriService.deleteKanri(this.data)
     .then(res => {
